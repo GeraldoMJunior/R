2fe938fbe4ad0f8f1b4796702a2231618e9e03da
gm2j <35813010+GeraldoMJunior@users.noreply.github.com>
2021-12-02 15:46:04 -0300

    Add files via upload

---
+++ cod-004.r
@@ -0,0 +1,14 @@
+# Learning R Language
+# Vetores: Objeto de dados básico
+# Vetores com múltiplos elementos
+# -------------------------------
+
+v1 <- 1:10
+v2 <- seq(5, 9, by = 0.5)
+v3 <- c("Seg", "Ter", 'Qua')
+
+cat(v1)
+cat("\n")
+cat(v2)
+cat("\n")
+cat(v3)
