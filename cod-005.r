f66f07eef80d784ac1482470d76122edd0c6ced4
gm2j <35813010+GeraldoMJunior@users.noreply.github.com>
2021-12-02 15:51:36 -0300

    Add files via upload

---
+++ cod-005.r
@@ -0,0 +1,17 @@
+# Learning R Language
+# Vetores: Objeto de dados básico
+# Acessando itens de um vetor
+# -------------------------------
+
+v1 <- c("Seg", "Ter", "Qua", "Qui", "Sex")
+
+cat(v1)
+cat("\n")
+
+cat(v1[3])
+cat("\n")
+
+cat(v1[5])
+cat("\n")
+
+cat(v1[1:3])
