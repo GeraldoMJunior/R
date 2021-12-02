b2b2440c69981b1529365f203432e3d49dfb4703
gm2j <35813010+GeraldoMJunior@users.noreply.github.com>
2021-12-02 16:09:11 -0300

    Add files via upload

---
+++ cod-007.r
@@ -0,0 +1,17 @@
+# Learning R Language
+# Vetores: Objeto de dados básico
+# Manipulação de Vetores
+# -------------------------------
+
+v1 <- c(1, 3, 5, 7)
+v2 <- c(2, 4, 6, 8)
+
+cat("Vetores completos\n")
+print(v1)
+print(v2)
+
+cat("Somar os dois vetores\n")
+print(v1 + v2)
+
+cat("Multipicação de um número e um vetor\n")
+print(3 * v2)
